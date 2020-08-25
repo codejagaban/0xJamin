32c6001cde0d95e49ceabd65439a3310c3ee7052
codekyd <okpusjamin@gmail.com>
2020-08-25 19:21:51 +0100

    add app data

---
+++ src/components/Data/WorkData.js
@@ -0,0 +1,35 @@
+const WorkData = [
+  {
+    title: 'Academia Publishing',
+    description: 'A  publishing platform for academic journals and articles',
+    link: '',
+    imageUrl: '',
+    technologies: ['React','Redux', 'Javascript', 'Gatsby', 'GraphQL'],
+  },
+  {
+    title: 'MBBOV',
+    description: 'A web-based microfinance banking system.',
+     link: '',
+    imageUrl: '',
+    technologies: ['React', 'Javascript', 'HTML', 'CSS & SASS',],
+  },
+  {
+    title: 'GasHub',
+    description: 'A GeoSpatial Representation of possible gas locations in Niger Delta.',
+     link: '',
+    imageUrl: '',
+    technologies: ['React', 'GeoJSON', 'Javascript',],
+  },
+  {
+    title: 'Intellect Scientific Information',
+    description: 'A multi-disciplinary bibliographic database service for academic Journals.',
+     link: '',
+    imageUrl: '',
+    type: ['project'],
+    technologies: ['React', 'Redux', 'Javascript', 'APIs'],
+  }
+
+  ]
+
+
+  export default  WorkData
