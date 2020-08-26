f7738171dc25974c7edee5d80f256ad33abe1d00
codekyd <okpusjamin@gmail.com>
2020-08-26 07:17:37 +0100

    add analytics

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,18 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+const React = require("react")
+export const onRenderBody = ({ setHeadComponents }, pluginOptions) => {
+  setHeadComponents([
+    <>
+      <script async key={'Globlagtag'} src="https://www.googletagmanager.com/gtag/js?id=UA-139158593-2"></script>
+      <script key={'gtag'} dangerouslySetInnerHTML={{ __html: ` window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
 
-// You can delete this file if you're not using it
+        gtag('config', 'UA-139158593-2');`}}>
+
+      </script>
+
+
+    </>
+
+  ])
+}
