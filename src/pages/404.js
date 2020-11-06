ee3afccc27d9e3823b4d5b7e563cba3c83688cc3
codekyd <okpusjamin@gmail.com>
2020-11-06 08:21:58 +0100

    :sparkles: feat: implement 404 error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,13 +2,26 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Button from "../components/Button/Button"
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+    <Layout>
+        <SEO title="404: Not found" />
+        <div className="greeting" style={{lineHeight: '1.2'}}>404</div>
+        <div className="container">
+            <div className="content">
+                <h1>NOT FOUND</h1>
+                <p>We couldn't find the page you're trying access,
+                    Try something else or
+                </p>
+                <Button
+                    content={"Go Home"}
+                    linkTo={"/"}
+                />
+
+            </div>
+        </div>
+    </Layout>
 )
 
 export default NotFoundPage
