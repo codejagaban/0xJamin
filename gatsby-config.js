635633c1900306c43d0ff2a0864fd775cc877372
codekyd <okpusjamin@gmail.com>
2020-11-06 08:33:56 +0100

    :zap: feat: add SW for offline mode

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,6 +30,12 @@ module.exports = {
     `gatsby-plugin-sharp`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        precachePages: [`/`,`/about/`,`/works/`, `/contact`],
+      },
+    },
+
   ],
 }
