f145255905f162d626584ca54c64bd0fd101f6a6
Trust <okpusjamin@gmail.com>
2023-07-26 12:20:10 +0100

    fix: fix typo error

--- src/data/home.ts
+++ src/data/home.ts
@@ -1,5 +1,5 @@
 export default  {
   greeting: "Hi I'm",
   name: "Trust Jamin",
-  intro: "Frontend Engineer who loves the artistic combination of code and design, I like building user centered applications so I ampassionate in making the web easily accessible for everyone."
+  intro: "Frontend Engineer who loves the artistic combination of code and design, I like building user centered applications so I am passionate in making the web easily accessible for everyone."
 };
