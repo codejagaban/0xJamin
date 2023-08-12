a2680cffed47603163113d653f0bbe6490191e0e
Jamin <okpusjamin@gmail.com>
2023-08-12 21:31:47 +0100

    refactor: update my name

--- src/data/home.ts
+++ src/data/home.ts
@@ -1,6 +1,6 @@
 export default  {
   greeting: "Hi I'm",
-  name: "Trust Jamin",
+  name: "Jamin (Jay-min)",
   intro: "Software Engineer and Developer Advocate🥑, I enjoy creating products that improve the quality of life for their users and solves problems. Having an affinity for helping others grow and become better, I lead a number of developer initiatives and communities across Africa and also share my knowledge through technical content and public speaking."
 
 };
